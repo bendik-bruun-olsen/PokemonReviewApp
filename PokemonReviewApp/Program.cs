c4f56f651e3dbbd4904267467555fe03dd6b6e75
Bendik Bruun-Olsen <bendik.bruun-olsen@bouvet.no>
2024-07-12 09:04:34 +0200

    Changing Swagger to be available in production

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -52,25 +52,25 @@ void SeedData(IHost app)
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-//app.UseSwagger();
 //if (app.Environment.IsDevelopment())
 //{
+//    app.UseSwagger();
 //    app.UseSwaggerUI();
 //}
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseSwaggerUI(options =>
-//    {
-//        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-//        options.RoutePrefix = string.Empty;
-//    });
-//}
+
+app.UseSwagger();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+}
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 
 app.UseHttpsRedirection();
